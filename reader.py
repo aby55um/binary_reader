06dad9a66675446a324955df015bd8a47c55dc4a
dbu99r@protonmail.com <dbu99r@protonmail.com>
2025-11-25 07:28:57 +0100

    file open button added

---
+++ reader.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+from tkinter import filedialog
+
+def choose_file(event=None):
+	filename = filedialog.askopenfilename()
+	print('Selected: ', filename)
+
+root = tk.Tk()
+
+root.title("Binary reader")
+root.geometry("800x600")
+
+button = tk.Button(root, text='Open', command=choose_file)
+button.pack()
+
+root.mainloop()
