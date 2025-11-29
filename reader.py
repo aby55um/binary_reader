f425e1a80006e5921649869f9e937b3e4fa1722e
dbu99r@protonmail.com <dbu99r@protonmail.com>
2025-11-29 04:42:28 +0100

    Formatted the binary display

--- reader.py
+++ reader.py
@@ -3,16 +3,29 @@ from tkinter import filedialog
 
 def choose_file(event=None):
 	filename = filedialog.askopenfilename()
-	entry.delete(0,tk.END)
+	text.delete('1.0',tk.END)
 	print('Selected: ', filename)
 	f = open(filename, 'rb')
 	byte = f.read(1)
-	file_string = ''
+	file_string = '  '
+	ascii_string = ''
+	newchar = 0
 	while byte:
-		print(f'{ord(byte):02x}', end=' ')
-		file_string = file_string + f'{ord(byte):02x}'
+		file_string = file_string + f'{ord(byte):02x} '
+		if ord(byte)>32 and ord(byte)<126:
+			ascii_string = ascii_string + chr(ord(byte))
+		else:
+			ascii_string = ascii_string + '.'
 		byte = f.read(1)
-	entry.insert(0,file_string)
+		newchar += 1
+		if newchar == 7:
+			file_string = file_string + '  '
+		if newchar == 15:
+			file_string = file_string + '   ' + ascii_string + '\n  '
+			ascii_string = '' 
+			newchar = 0
+	file_string = file_string + (50-3*newchar-int(newchar/7)*2+int(newchar/14)*2)*' ' + ascii_string + '\n'
+	text.insert("1.0",file_string)
 
 root = tk.Tk()
 
@@ -21,11 +34,8 @@ root.geometry("800x600")
 
 button = tk.Button(root, text='Open', command=choose_file)
 button.place(x=50, y=20)
-#button.pack()
 
-#tk.Entry(justify='center').pack()
-entry = tk.Entry()
-entry.place(x=50,y=50, width=700, height=500)
+text = tk.Text()
+text.place(x=50,y=50, width=700, height=500)
 
-
-root.mainloop()
+root.mainloop()  
